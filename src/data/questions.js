d8c40fe8f40fe685fc18547abbd8530e9acea61e
Kaio Brenner <kaiobrenner460@protonmail.com>
2023-05-16 20:27:33 -0300

    first commit

---
+++ src/data/questions.js
@@ -0,0 +1,55 @@
+const questionsData = [
+  {
+    question: "Quanto é 1 + 2",
+    answers: [
+      {
+        text: "3",
+        correct: true,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Quanto é 1 + 2",
+    answers: [
+      {
+        text: "3",
+        correct: true,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Quanto é 1 + 2",
+    answers: [
+      {
+        text: "3",
+        correct: true,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+    ],
+  },
+];
+
+export default questionsData;
